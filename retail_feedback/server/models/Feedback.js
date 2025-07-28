aed6df6b48d217b1ecc03cf90aebf98ae1e8edeb
Juan Salamanca <est.juanf.salamanca@unimilitar.edu.co>
2025-07-27 21:38:28 -0500

    Property update for the Mongoose Model

--- retail_feedback/server/models/Feedback.js
+++ retail_feedback/server/models/Feedback.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const feedbackSchema = new mongoose.Schema({
     location: String,
     availability: Number,
-    experience: Number,
+    staff: String,
+    cleanliness: Number,
+    satisfaction: Number,
     comment: String,
     createdAt: { type: Date, default: Date.now }
 });
-
 module.exports = mongoose.model('Feedback', feedbackSchema);
