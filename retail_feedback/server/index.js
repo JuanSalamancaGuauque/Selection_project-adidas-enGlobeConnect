f7e792b4697190bd63669b608bc0beb5d171a2c0
Juan Salamanca <est.juanf.salamanca@unimilitar.edu.co>
2025-07-30 20:10:37 -0500

    API Fix

--- retail_feedback/server/index.js
+++ retail_feedback/server/index.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/retail_feedback',
-{
-    useNewUrlPaser: true, useUnifiedTopology: true
-});
+mongoose.connect('mongodb://localhost:27017/retail_feedback')
 
 const Feedback = require('./models/Feedback');
 
 app.post('/api/feedback', async (req, res) => {
+    try{
     const data = new Feedback(req.body);
     await data.save();
+    res.status(201).send({message: 'save'});
+} catch (error){
+    res.status(400).send({message: 'error'});
+}
 });
 
 app.get('/api/feedback', async (req, res) => {
